2b54859dd56d82e5fe59e17314bde83bdcc6ab26
Ryan LaBarge <ryanclabarge@gmail.com>
2017-08-29 21:04:24 -0400

    removing extra files

--- UseArguments.java
+++
@@ -1,8 +0,0 @@
-public class UseArguments {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello "+args[0]+" and "+args[1]+". Welcome to Java.");
-	}
-
-}
