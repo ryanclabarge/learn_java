642ab12f433b8689e79f09494d9e22747f31c501
Ryan LaBarge <ryanclabarge@gmail.com>
2017-09-07 18:45:11 -0400

    added Gui

---
+++ Gui.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+
+public class Gui {
+	public static void main(String[] args){ //public is the access modifier void is the return type
+		int t = Integer.parseInt(JOptionPane.showInputDialog(null,"Please input an integer"));
+		int sum = (t*(t+1))/2;
+		JOptionPane.showMessageDialog(null,sum);
+
+	}
+}
