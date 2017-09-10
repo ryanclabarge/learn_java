29984e29fb690889c642ea52322902b82e28b526
Ryan LaBarge <ryanclabarge@gmail.com>
2017-09-10 12:56:59 -0400

    Added Mortgage

--- Mortgage.java
+++ Mortgage.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
+import java.text.NumberFormat;
+
 public class Mortgage {
 	public static double Payment(double a, double b, double c){//this calculates the payment
-		double payment = ((a*(b/12.0))/(1.0-(1.0/(Math.pow((1.0+(b/12.0)),(c*12.0))))));
+		//double payment = ((a*(b/12.0))/(1.0-(1.0/(Math.pow((1.0+(b/12.0)),(c*12.0))))));
+		double r = (b/100.0);
+		double x = (1.0+(r/12.0));
+		double y = (c*12.0);
+		double temp = (1.0/(Math.pow(x,y)));
+		double payment = ((a*(r/12.0))/(1-temp));
 		return payment;
 	}
 	public static double Future(double p, double c){
@@ -11,11 +19,19 @@ public class Mortgage {
 		return (a-b);
 	}
 	public static void main(String[] args){
-		double a = Double.parseDouble(args[0]);
-		double b = Double.parseDouble(args[1]);
-		double c = Double.parseDouble(args[2]);
-		System.out.println(args[0]);
-		System.out.println(Future(Payment(a,b,c),c));
+		Scanner scan = new Scanner(System.in);//creates a Scanner object
+		System.out.println("Enter the starting amount of the Mortgage:");
+		double a = scan.nextDouble();//Gets the Mortgage
+		System.out.println("Enter the length of the term in years:");
+		double c = scan.nextDouble();//Gets the Term in years
+		System.out.println("Enter the interest rate:");
+		double b = scan.nextDouble();//Gets the interest rate at a number
+		NumberFormat formatter = NumberFormat.getCurrencyInstance();
+		System.out.println("Princapal: " +formatter.format(a));
+		System.out.println("Future Value: "+formatter.format(Future(Payment(a,b,c),c)));
+		System.out.println("Interest Rate: "+b+"%");
+		System.out.println("Interest Charge: "+formatter.format(Interest(Future(Payment(a,b,c),c),a)));
+		System.out.println("Payment: "+formatter.format(Payment(a,b,c)));
 	}
 }
 
