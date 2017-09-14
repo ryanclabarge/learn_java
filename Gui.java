bd510d438035247af630e1d38f5cde4bc38691c6
Ryan LaBarge <ryanclabarge@gmail.com>
2017-09-14 17:52:10 -0400

    edited Gui.java

--- Gui.java
+++ Gui.java
@@ -4,7 +4,7 @@ public class Gui {
 	public static void main(String[] args){ //public is the access modifier void is the return type
 		int t = Integer.parseInt(JOptionPane.showInputDialog(null,"Please input an integer"));
 		int sum = (t*(t+1))/2;
-		JOptionPane.showMessageDialog(null,sum);
+		JOptionPane.showMessageDialog(null,sum,"Sum",JOptionPane.PLAIN_MESSAGE);
 
 	}
 }
